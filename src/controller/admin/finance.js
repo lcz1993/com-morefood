a389ea197285603faaede368fd84350b8dd7e255
赵思岩 <724703549@qq.com>
2018-11-14 15:18:32 +0800

    赵思岩

--- src/controller/admin/finance.js
+++ src/controller/admin/finance.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-multiple-empty-lines,no-trailing-spaces,indent,no-undef,space-before-blocks,comma-spacing,spaced-comment,no-unused-vars,no-unreachable,key-spacing,camelcase,space-infix-ops,no-use-before-define,no-const-assign,eqeqeq,keyword-spacing,prefer-const */
 // +----------------------------------------------------------------------
 // | CmsWing [ 网站内容管理框架 ]
 // +----------------------------------------------------------------------
@@ -25,24 +26,136 @@ module.exports = class extends think.cmswing.admin {
    * 财务日志
    */
   async logAction() {
-    const list = await this.model('balance_log').order('id DESC').page(this.get('page') || 1, 20).countSelect();
-    const html = this.pagination(list);
-    this.assign('pagerData', html);
-    for (const item of list.data) {
-      const restaurant = await this.model('restaurant').field('name').find(item.restaurant_id);
-      item.restaurantName = restaurant.name;
-      const user = await this.model('wx_user').field('nickname').find(item.user_id);
-      item.username = user.nickname;
-      const note = JSON.parse(item.note);
-      const foodArr = note.foodList;
-      let str = '';
-      for (const i in foodArr) {
-        str += foodArr[i].title + '(' + foodArr[i].qty + '份￥' + foodArr[i].price + '),';
+      const startTime = this.get('startTime');
+      const endTime = this.get('endTime');
+      const restaurant_id = this.get('restaurant_id');
+      const restautantId = this.user.restaurant_id;
+      const start=new Date();
+      start.setHours(0);
+      start.setMinutes(0);
+      start.setSeconds(0);
+      start.setMilliseconds(0);
+      const todayStartTime=Date.parse(start)/1;
+      console.log(todayStartTime); //Mon Dec 04 2017 00:00:00 GMT+0800 (中国标准时间)
+      const map = {};
+      let restaurantArr = [];
+      if(!think.isEmpty(restaurant_id)){
+          map.restaurant_id = restaurant_id;
       }
-      item.note = str;
-    }
-    this.assign('list', list.data);
-    this.meta_title = '财务日志';
-    return this.display();
+      if(restautantId != 0){
+          map.restaurant_id = restautantId;
+          restaurantArr = await this.model('restaurant').where({id:restautantId}).select();
+      }else{
+          restaurantArr = await this.model('restaurant').select();
+      }
+      if (think.isEmpty(startTime) && !think.isEmpty(endTime)) {
+          const t1 = new Date(endTime.replace(/-/g, '/')).getTime();
+          map.time = ['BETWEEN',0,t1];
+      }else if (think.isEmpty(endTime) && !think.isEmpty(startTime)) {
+          const t = new Date(startTime.replace(/-/g, '/')).getTime();
+          map.time = ['BETWEEN',t,new Date().getTime()];
+      }else if (think.isEmpty(endTime) && think.isEmpty(startTime)) {
+          map.time = ['>=',todayStartTime];
+      }else{
+          const t = new Date(startTime.replace(/-/g, '/')).getTime();
+          const t1 = new Date(endTime.replace(/-/g, '/')).getTime();
+          map.time = ['BETWEEN',t,t1];
+      }
+      console.log(map);
+          const list = await this.model('balance_log').where(map).order('id DESC').page(this.get('page') || 1, 20).countSelect();
+          const html = this.pagination(list);
+          this.assign('pagerData', html);
+          for (const item of list.data) {
+              const restaurant = await this.model('restaurant').field('name').find(item.restaurant_id);
+              item.restaurantName = restaurant.name;
+              const user = await this.model('wx_user').field('nickname').find(item.user_id);
+              item.username = user.nickname;
+              const note = JSON.parse(item.note);
+              const foodArr = note.foodList;
+              let str = '';
+              for (const i in foodArr) {
+                  str += foodArr[i].title + '(' + foodArr[i].qty + '份￥' + foodArr[i].price + '),';
+              }
+              item.note = str;
+          }
+          this.assign('list', list);
+          this.assign('restaurantArr', restaurantArr);
+
+      this.meta_title = '财务日志';
+      return this.display();
   }
+    /**
+     * 打印报表
+     */
+    async printAction() {
+        const data1 = this.get();
+        const startTime = this.get('startTime');
+        const endTime = this.get('endTime');
+        const restaurant_id = this.get('restaurant_id');
+        const start=new Date();
+        start.setHours(0);
+        start.setMinutes(0);
+        start.setSeconds(0);
+        start.setMilliseconds(0);
+        const todayStartTime=Date.parse(start)/1;
+        const map = {};
+        if(!think.isEmpty(restaurant_id)){
+            map.restaurant_id = restaurant_id;
+        }
+        if (think.isEmpty(startTime) && !think.isEmpty(endTime)) {
+            const t1 = new Date(endTime.replace(/-/g, '/')).getTime();
+            map.time = ['BETWEEN',0,t1];
+        }else if (think.isEmpty(endTime) && !think.isEmpty(startTime)) {
+            const t = new Date(startTime.replace(/-/g, '/')).getTime();
+            map.time = ['BETWEEN',t,new Date().getTime()];
+        }else if (think.isEmpty(endTime) && think.isEmpty(startTime)) {
+            map.time = ['>=',todayStartTime];
+        }else{
+            const t = new Date(startTime.replace(/-/g, '/')).getTime();
+            const t1 = new Date(endTime.replace(/-/g, '/')).getTime();
+            map.time = ['BETWEEN',t,t1];
+        }
+
+        console.log(map);
+        const list = await this.model('balance_log').where(map).order('id ASC').select();
+        console.log(list);
+        const foodList = [];
+        let amount = 0;
+        for (const i in list) {
+            const goods = list[i];
+            const restaurant = await this.model('restaurant').field('name').find(goods.restaurant_id);
+            goods.restaurantName = restaurant.name;
+            const user = await this.model('wx_user').field('nickname').find(goods.user_id);
+            goods.username = user.nickname;
+            const note = JSON.parse(goods.note);
+            for (const i in note.foodList) {
+                const a = note.foodList[i];
+                const title = a.title;
+                const qty = a.qty;
+                const price = a.price;
+                const unit_price = a.unit_price;
+
+                const b = {
+                    title: title,
+                    num: qty,
+                    total_price: price,
+                    restaurantName: goods.restaurantName,
+                    username: goods.username,
+                    time: goods.time,
+                    unit_price: unit_price
+                };
+
+                amount += note.price;
+                foodList.push(b);
+            }
+        }
+        const data = {
+            title:  '报表统计',
+            goodsList: foodList,
+            countprice: amount
+        };
+        console.log(foodList);
+        this.assign('data', data);
+        return this.display();
+    };
 };
