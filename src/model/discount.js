dcf4d1217a68791ea55064c96d9b49bfab1554f9
赵思岩 <724703549@qq.com>
2019-01-02 17:54:59 +0800

    打印票据

--- src/model/discount.js
+++ src/model/discount.js
@@ -62,148 +62,74 @@ module.exports = class extends think.Model {
   }
 
   /**
-     * 获取当前用户优惠信息
+     * 获取当前可用优惠信息
      * @returns {Promise<*>}
      */
   async getcoupon(userId, price, restaurantId) {
-    const couponList = [];
     const map = {};
     map.type_id = 6;
-    map.usage_restriction = 1;
     map.restaurant_id = restaurantId;
-    const couponArr = await this.model('order').find({ user_id: userId });
-    if (think.isEmpty(couponArr) || couponArr.status == 6) {
-      const coupon = await this.model('discount').find(map);
-      const money = await this.model('discount').getmoney(price, restaurantId);
-      const food = await this.model('discount').getgoods(price, restaurantId);
-      if (coupon.min_price <= price) {
-        const b = {
-          id: coupon.id,
-          name: coupon.name,
-          desc: coupon.desc,
-          start_time: coupon.start_time,
-          end_time: coupon.end_time,
-          status: coupon.status,
-          type_id: coupon.type_id,
-          percent: coupon.percent,
-          cut_price: coupon.cut_price,
-          min_price: coupon.min_price,
-          restaurant_id: coupon.restaurant_id,
-          medu_id: coupon.medu_id,
-          max_count: coupon.max_count,
-          pull_count: coupon.pull_count,
-          use_count: coupon.use_count,
-          usage_restriction: coupon.usage_restriction,
-          distribution_platform: coupon.distribution_platform,
-          food_name: food,
-          cashback: money
-        };
-        couponList.push(b);
+    const order = await this.model('order').where({ user_id: userId, status: ['<', 6]}).select();
+    const discountList = [];
+    if (order.length == 0) {
+      // shoudan
+      let shoudan = await this.where({
+        type_id: 6,
+        start_time: ['<', new Date().getTime()],
+        end_time: ['>', new Date().getTime()],
+        restaurant_id: restaurantId,
+        distribution_platform: 2,
+        min_price: ['<', price]
+      }).select();
+      if (shoudan.length == 0) {
+        shoudan = await this.where({
+          type_id: 6,
+          start_time: ['<', new Date().getTime()],
+          end_time: ['>', new Date().getTime()]
+        }).select();
+      }
+      if (!think.isEmpty(shoudan)) {
+        discountList.push(shoudan[0]);
       }
     } else {
-      const map = {};
-      map.type_id = [1, 5];
-      map.restaurant_id = restaurantId;
-      const discount = await this.model('discount').where(map).select();
-      for (const item of discount) {
-        const money = await this.model('discount').getmoney(price, restaurantId);
-        const food = await this.model('discount').getgoods(price, restaurantId);
-        if (item.min_price <= price) {
-          const b = {
-            id: item.id,
-            name: item.name,
-            desc: item.desc,
-            start_time: item.start_time,
-            end_time: item.end_time,
-            status: item.status,
-            type_id: item.type_id,
-            percent: item.percent,
-            cut_price: item.cut_price,
-            min_price: item.min_price,
-            restaurant_id: item.restaurant_id,
-            medu_id: item.medu_id,
-            max_count: item.max_count,
-            pull_count: item.pull_count,
-            use_count: item.use_count,
-            usage_restriction: item.usage_restriction,
-            distribution_platform: item.distribution_platform,
-            food_name: food,
-            cashback: money
-          };
-          couponList.push(b);
-        }
+      // 满减或满折
+      const man = await this.where({
+        type_id: ['like', ['1', '5']],
+        start_time: ['<', new Date().getTime()],
+        end_time: ['>', new Date().getTime()],
+        restaurant_id: restaurantId,
+        distribution_platform: 2,
+        min_price: ['<', price]
+      }).select();
+      if (!think.isEmpty(man)) {
+        discountList.push(man[0]);
       }
     }
-
-    return couponList;
-  }
-
-  /**
-     * 获取当前用户所以可用的优惠信息
-     */
-  async getList(userId, price, restaurantId) {
-    const couponList = [];
-    const couponlist = await this.model('coupon').where({ user_id: userId }).select();
-    for (const i of couponlist) {
-      const id = i.discount_id;
-      const discountlist = await this.model('discount').where({ id: id }).select();
-      for (const item of discountlist) {
-        const food = await this.model('discount').getgoods(price, restaurantId);
-        const money = await this.model('discount').getmoney(price, restaurantId);
-        if (item.min_price <= price) {
-          const personal = {
-            id: item.id,
-            name: item.name,
-            desc: item.desc,
-            start_time: item.start_time,
-            end_time: item.end_time,
-            status: item.status,
-            type_id: item.type_id,
-            percent: item.percent,
-            cut_price: item.cut_price,
-            min_price: item.min_price,
-            restaurant_id: item.restaurant_id,
-            medu_id: item.medu_id,
-            max_count: item.max_count,
-            pull_count: item.pull_count,
-            use_count: item.use_count,
-            usage_restriction: item.usage_restriction,
-            distribution_platform: item.distribution_platform,
-            food_name: food,
-            cashback: money
-          };
-          couponList.push(personal);
-        }
-      }
+    // 满赠
+    const manzeng = await this.where({
+      type_id: 3,
+      start_time: ['<', new Date().getTime()],
+      end_time: ['>', new Date().getTime()],
+      restaurant_id: restaurantId,
+      distribution_platform: 2,
+      min_price: ['<', price]
+    }).select();
+    if (!think.isEmpty(manzeng)) {
+      discountList.push(manzeng[0]);
     }
-    const discount = await this.model('discount').where({ restaurant_id: restaurantId }).select();
-    const food = await this.model('discount').getgoods(price, restaurantId);
-    const money = await this.model('discount').getmoney(price, restaurantId);
-    for (const i of discount) {
-      const platform = {
-        id: i.id,
-        name: i.name,
-        desc: i.desc,
-        start_time: i.start_time,
-        end_time: i.end_time,
-        status: i.status,
-        type_id: i.type_id,
-        percent: i.percent,
-        cut_price: i.cut_price,
-        min_price: i.min_price,
-        restaurant_id: i.restaurant_id,
-        medu_id: i.medu_id,
-        max_count: i.max_count,
-        pull_count: i.pull_count,
-        use_count: i.use_count,
-        usage_restriction: i.usage_restriction,
-        distribution_platform: i.distribution_platform,
-        food_name: food,
-        cashback: money
-      };
-      couponList.push(platform);
+    // 返现
+    const fanxian = await this.where({
+      type_id: 4,
+      start_time: ['<', new Date().getTime()],
+      end_time: ['>', new Date().getTime()],
+      restaurant_id: restaurantId,
+      distribution_platform: 2,
+      min_price: ['<', price]
+    }).select();
+    if (!think.isEmpty(fanxian)) {
+      discountList.push(fanxian[0]);
     }
-    return couponList;
+    return discountList;
   }
 
   /**
